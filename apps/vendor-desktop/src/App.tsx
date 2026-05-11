c7a620c8b1d8c9e628202dd71927d8eb753b3661
disateq-dev <disateq.dev@gmail.com>
2026-05-11 06:07:52 -0500

    feat(ticket): extract TicketWorkspace from App

--- apps/vendor-desktop/src/App.tsx
+++ apps/vendor-desktop/src/App.tsx
@@ -1,16 +1,12 @@
-import { TicketGrid } from "./domains/ticket/components/TicketGrid";
 import { AppShell } from "./layout/AppShell";
 import { SalesWorkspace } from "./modules/sales/SalesWorkspace";
+import { TicketWorkspace } from "./modules/ticket/TicketWorkspace";
 
 export default function App() {
   return (
     <AppShell>
       <SalesWorkspace />
-
-      {/* RIGHT */}
-      <section className="w-[420px]">
-        <TicketGrid />
-      </section>
+      <TicketWorkspace />
     </AppShell>
   );
 }
---
+++ apps/vendor-desktop/src/modules/ticket/TicketWorkspace.tsx
@@ -0,0 +1,9 @@
+import { TicketGrid } from "../../domains/ticket/components/TicketGrid";
+
+export function TicketWorkspace() {
+  return (
+    <section className="w-[420px]">
+      <TicketGrid />
+    </section>
+  );
+}
