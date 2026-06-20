966b93623aeaf5d1e1b0f01db64763e085ddef3c
disateq-dev <disateq.dev@gmail.com>
2026-06-20 06:16:32 -0500

    fix(farmacia): alinear argumentos de invoke() a camelCase nativo de Tauri 2.x

--- apps/vendor-desktop/src/domains/farmacia/farmacia.service.ts
+++ apps/vendor-desktop/src/domains/farmacia/farmacia.service.ts
@@ -60,30 +60,30 @@ export async function crearProductoGenerico(input: CrearProductoGenericoInput): 
   return invoke<string>('crear_producto_generico', {
     ifa: input.ifa,
     concentracion: input.concentracion,
-    forma_farmaceutica: input.formaFarmaceutica,
-    categoria_farmacia: input.categoriaFarmacia,
-    permite_fraccion: input.permiteFraccion,
+    formaFarmaceutica: input.formaFarmaceutica,
+    categoriaFarmacia: input.categoriaFarmacia,
+    permiteFraccion: input.permiteFraccion,
   })
 }
 
 export async function obtenerProductosGenericos(filtroIfa?: string): Promise<ProductoGenerico[]> {
   return invoke<ProductoGenerico[]>('obtener_productos_genericos', {
-    filtro_ifa: filtroIfa ?? null,
+    filtroIfa: filtroIfa ?? null,
   })
 }
 
 export async function crearProductoComercial(input: CrearProductoComercialInput): Promise<string> {
   return invoke<string>('crear_producto_comercial', {
-    producto_generico_id: input.productoGenericoId,
-    nombre_comercial: input.nombreComercial,
-    nombre_fabricante: input.nombreFabricante,
-    nombre_titular: input.nombreTitular ?? null,
-    pais_origen: input.paisOrigen ?? null,
-    registro_sanitario: input.registroSanitario ?? null,
-    codigo_digemid: input.codigoDIGEMID ?? null,
-    condicion_venta: input.condicionVenta,
-    requiere_lote: input.requiereLote,
-    requiere_cadena_frio: input.requiereCadenaFrio,
+    productoGenericoId: input.productoGenericoId,
+    nombreComercial: input.nombreComercial,
+    nombreFabricante: input.nombreFabricante,
+    nombreTitular: input.nombreTitular ?? null,
+    paisOrigen: input.paisOrigen ?? null,
+    registroSanitario: input.registroSanitario ?? null,
+    codigoDigemid: input.codigoDIGEMID ?? null,
+    condicionVenta: input.condicionVenta,
+    requiereLote: input.requiereLote,
+    requiereCadenaFrio: input.requiereCadenaFrio,
   })
 }
 
@@ -92,75 +92,75 @@ export async function obtenerProductosComerciales(
   soloActivos?: boolean,
 ): Promise<ProductoComercial[]> {
   return invoke<ProductoComercial[]>('obtener_productos_comerciales', {
-    filtro_nombre: filtroNombre ?? null,
-    solo_activos: soloActivos ?? null,
+    filtroNombre: filtroNombre ?? null,
+    soloActivos: soloActivos ?? null,
   })
 }
 
 export async function crearPresentacion(input: CrearPresentacionInput): Promise<string> {
   return invoke<string>('crear_presentacion', {
-    producto_comercial_id: input.productoComercialId,
+    productoComercialId: input.productoComercialId,
     descripcion: input.descripcion,
-    fraccion_digemid: input.fraccionDIGEMID,
-    unidad_conteo: input.unidadConteo,
-    factor_conversion_base: input.factorConversionBase,
-    codigo_barras: input.codigoBarras ?? null,
-    proveedor_habitual_id: input.proveedorHabitualId ?? null,
-    costo_compra: input.costoCompra ?? null,
+    fraccionDigemid: input.fraccionDIGEMID,
+    unidadConteo: input.unidadConteo,
+    factorConversionBase: input.factorConversionBase,
+    codigoBarras: input.codigoBarras ?? null,
+    proveedorHabitualId: input.proveedorHabitualId ?? null,
+    costoCompra: input.costoCompra ?? null,
   })
 }
 
 export async function obtenerPresentaciones(productoComercialId: string): Promise<PresentacionComercial[]> {
   return invoke<PresentacionComercial[]>('obtener_presentaciones', {
-    producto_comercial_id: productoComercialId,
+    productoComercialId,
   })
 }
 
 export async function crearNodo(input: CrearNodoInput): Promise<string> {
   return invoke<string>('crear_nodo', {
-    presentacion_id: input.presentacionId,
-    nodo_padre_id: input.nodoPadreId ?? null,
-    nombre_forma_venta: input.nombreFormaVenta,
-    tipo_forma_venta: input.tipoFormaVenta,
-    unidades_en_nodo_padre: input.unidadesEnNodoPadre ?? null,
-    unidades_base: input.unidadesBase,
-    es_vendible: input.esVendible,
-    es_comprable: input.esComprable,
-    descripcion_promo: input.descripcionPromo ?? null,
+    presentacionId: input.presentacionId,
+    nodoPadreId: input.nodoPadreId ?? null,
+    nombreFormaVenta: input.nombreFormaVenta,
+    tipoFormaVenta: input.tipoFormaVenta,
+    unidadesEnNodoPadre: input.unidadesEnNodoPadre ?? null,
+    unidadesBase: input.unidadesBase,
+    esVendible: input.esVendible,
+    esComprable: input.esComprable,
+    descripcionPromo: input.descripcionPromo ?? null,
   })
 }
 
 export async function obtenerNodosFraccionamiento(presentacionId: string): Promise<NodoFraccionamiento[]> {
   return invoke<NodoFraccionamiento[]>('obtener_nodos_fraccionamiento', {
-    presentacion_id: presentacionId,
+    presentacionId,
   })
 }
 
 export async function crearProveedor(input: CrearProveedorInput): Promise<string> {
   return invoke<string>('crear_proveedor', {
-    razon_social: input.razonSocial,
+    razonSocial: input.razonSocial,
     ruc: input.ruc ?? null,
-    nombre_contacto: input.nombreContacto ?? null,
+    nombreContacto: input.nombreContacto ?? null,
     telefono: input.telefono ?? null,
-    condiciones_pago: input.condicionesPago ?? null,
+    condicionesPago: input.condicionesPago ?? null,
   })
 }
 
 export async function obtenerProveedores(soloActivos?: boolean): Promise<Proveedor[]> {
   return invoke<Proveedor[]>('obtener_proveedores', {
-    solo_activos: soloActivos ?? null,
+    soloActivos: soloActivos ?? null,
   })
 }
 
 export async function registrarLote(input: RegistrarLoteInput): Promise<string> {
   return invoke<string>('registrar_lote', {
-    presentacion_id: input.presentacionId,
-    numero_lote: input.numeroLote,
-    fecha_vencimiento: input.fechaVencimiento,
-    fecha_fabricacion: input.fechaFabricacion ?? null,
-    cantidad_ingresada: input.cantidadIngresada,
-    proveedor_id: input.proveedorId ?? null,
-    precio_compra: input.precioCompra ?? null,
+    presentacionId: input.presentacionId,
+    numeroLote: input.numeroLote,
+    fechaVencimiento: input.fechaVencimiento,
+    fechaFabricacion: input.fechaFabricacion ?? null,
+    cantidadIngresada: input.cantidadIngresada,
+    proveedorId: input.proveedorId ?? null,
+    precioCompra: input.precioCompra ?? null,
   })
 }
 
@@ -169,69 +169,69 @@ export async function resolverLoteFefo(
   unidadesRequeridas: number,
 ): Promise<AsignacionLote[]> {
   return invoke<AsignacionLote[]>('resolver_lote_fefo', {
-    presentacion_id: presentacionId,
-    unidades_requeridas: unidadesRequeridas,
+    presentacionId,
+    unidadesRequeridas,
   })
 }
 
 export async function obtenerLotesVigentes(presentacionId: string): Promise<Lote[]> {
   return invoke<Lote[]>('obtener_lotes_vigentes', {
-    presentacion_id: presentacionId,
+    presentacionId,
   })
 }
 
 export async function registrarMovimiento(input: RegistrarMovimientoInput): Promise<string> {
   return invoke<string>('registrar_movimiento', {
-    item_id: input.itemId,
+    itemId: input.itemId,
     tipo: input.tipo,
-    unidades_base: input.unidadesBase,
-    lote_id: input.loteId ?? null,
-    nodo_id: input.nodoId ?? null,
+    unidadesBase: input.unidadesBase,
+    loteId: input.loteId ?? null,
+    nodoId: input.nodoId ?? null,
     causa: input.causa,
-    referencia_id: input.referenciaId ?? null,
-    operador_id: input.operadorId,
-    runtime_id: input.runtimeId,
+    referenciaId: input.referenciaId ?? null,
+    operadorId: input.operadorId,
+    runtimeId: input.runtimeId,
   })
 }
 
 export async function crearServicioFarmacia(input: CrearServicioFarmaciaInput): Promise<string> {
   return invoke<string>('crear_servicio_farmacia', {
     nombre: input.nombre,
-    tipo_servicio: input.tipoServicio,
+    tipoServicio: input.tipoServicio,
     descripcion: input.descripcion ?? null,
-    duracion_minutos: input.duracionMinutos ?? null,
+    duracionMinutos: input.duracionMinutos ?? null,
   })
 }
 
 export async function obtenerServiciosFarmacia(soloActivos?: boolean): Promise<ServicioFarmacia[]> {
   return invoke<ServicioFarmacia[]>('obtener_servicios_farmacia', {
-    solo_activos: soloActivos ?? null,
+    soloActivos: soloActivos ?? null,
   })
 }
 
 export async function registrarEjecucionServicio(input: RegistrarEjecucionServicioInput): Promise<string> {
   return invoke<string>('registrar_ejecucion_servicio', {
-    servicio_id: input.servicioId,
-    operador_id: input.operadorId,
-    turno_id: input.turnoId ?? null,
-    pedido_id: input.pedidoId ?? null,
-    timestamp_inicio: input.timestampInicio,
-    timestamp_fin: input.timestampFin ?? null,
-    duracion_minutos: input.duracionMinutos ?? null,
+    servicioId: input.servicioId,
+    operadorId: input.operadorId,
+    turnoId: input.turnoId ?? null,
+    pedidoId: input.pedidoId ?? null,
+    timestampInicio: input.timestampInicio,
+    timestampFin: input.timestampFin ?? null,
+    duracionMinutos: input.duracionMinutos ?? null,
     observacion: input.observacion ?? null,
   })
 }
 
 export async function generarReporteDIGEMID(codigoEstab: string): Promise<ResultadoReporteDIGEMID> {
   return invoke<ResultadoReporteDIGEMID>('generar_reporte_digemid', {
-    codigo_estab: codigoEstab,
+    codigoEstab,
   })
 }
 
 export async function buscarProveedores(termino: string, soloActivos?: boolean): Promise<Proveedor[]> {
   const respuesta = await invoke<ProveedorRespuesta[]>('buscar_proveedores', {
     termino,
-    solo_activos: soloActivos ?? null,
+    soloActivos: soloActivos ?? null,
   })
   return respuesta.map((proveedor) => traducirProveedor(proveedor))
 }
@@ -250,27 +250,27 @@ export async function consultarRuc(ruc: string): Promise<DatosRuc> {
 export async function actualizarProveedor(input: ActualizarProveedorInput): Promise<void> {
   return invoke<void>('actualizar_proveedor', {
     id: input.id,
-    razon_social: input.razonSocial,
+    razonSocial: input.razonSocial,
     ruc: input.ruc ?? null,
-    nombre_contacto: input.nombreContacto ?? null,
+    nombreContacto: input.nombreContacto ?? null,
     telefono: input.telefono ?? null,
-    condiciones_pago: input.condicionesPago ?? null,
+    condicionesPago: input.condicionesPago ?? null,
   })
 }
 
 export async function registrarIngreso(input: RegistrarIngresoInput): Promise<string[]> {
   return invoke<string[]>('registrar_ingreso', {
-    proveedor_id: input.proveedorId,
-    operador_id: input.operadorId,
-    runtime_id: input.runtimeId,
+    proveedorId: input.proveedorId,
+    operadorId: input.operadorId,
+    runtimeId: input.runtimeId,
     lineas: input.lineas.map((linea) => ({
-      presentacion_id: linea.presentacionId,
+      presentacionId: linea.presentacionId,
       cantidad: linea.cantidad,
-      costo_unitario: linea.costoUnitario ?? null,
-      requiere_lote: linea.requiereLote,
-      numero_lote: linea.numeroLote ?? null,
-      fecha_vencimiento: linea.fechaVencimiento ?? null,
-      es_lote_generico: linea.esLoteGenerico,
+      costoUnitario: linea.costoUnitario ?? null,
+      requiereLote: linea.requiereLote,
+      numeroLote: linea.numeroLote ?? null,
+      fechaVencimiento: linea.fechaVencimiento ?? null,
+      esLoteGenerico: linea.esLoteGenerico,
     })),
   })
 }
